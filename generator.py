e16e1889d1ca6da448fdaf7424fcbabd7dd40f8c
seoilgun <seoilgun@gmail.com>
2018-09-11 18:14:27 +0900

    discriminator

--- generator.py
+++ generator.py
@@ -8,11 +8,14 @@ from tf_utils import weight_variable, bias_variable, dense_to_one_hot
 
 class Generator:
     def __init__(self, inputs, is_training, input_height, input_width, real_height, real_width, num_classes=10,
-                 slice=False):
+                 mode=2):
         with tf.variable_scope("generator"):
             print(inputs)
-            if not slice:
+            if mode == 1:
                 bg_transform, title_transform, credit_transform = self.transform_mixed(inputs, real_height, real_width)
+            elif mode == 2:
+                bg_transform, title_transform, credit_transform = self.transform_isotropic(inputs, real_height,
+                                                                                           real_width)
             else:
                 bg, title, credit = tf.split(inputs, [3, 4, 4], 3)
                 bg_transform = self.transform(bg, real_height, real_width)
@@ -76,6 +79,41 @@ class Generator:
 
         bg, title, credit = tf.split(inputs, [3, 4, 4], 3)
         bg_p, title_p, credit_p = tf.split(net, 3, 1)
+        bg_trans = transformer(bg, bg_p, (out_height, out_width))
+        title_trans = transformer(title, title_p, (out_height, out_width))
+        credit_trans = transformer(credit, credit_p, (out_height, out_width))
+        return bg_trans, title_trans, credit_trans
+
+    def transform_isotropic(self, inputs, out_height, out_width):
+        net = tf.layers.conv2d(inputs, 64, 7, 3, activation=tf.nn.relu)
+        net = tf.layers.max_pooling2d(net, 3, 2)
+        net = tf.layers.conv2d(net, 128, 3, activation=tf.nn.relu)
+        net = tf.layers.conv2d(net, 128, 3, activation=tf.nn.relu)
+        net = tf.layers.max_pooling2d(net, 3, 2)
+        net = tf.layers.conv2d(net, 256, 3, activation=tf.nn.relu)
+        net = tf.layers.conv2d(net, 256, 3, activation=tf.nn.relu)
+        net = tf.layers.conv2d(net, 128, 3, activation=tf.nn.relu)
+        net = tf.layers.conv2d(net, 128, 3, activation=tf.nn.relu)
+        net = tf.reduce_mean(net, [1, 2], name='global_pool')
+        net = tf.layers.dense(net, 36, activation=tf.nn.tanh)
+        net = tf.layers.dropout(net, 0.8)
+        initial = np.array([[1., 0, 0], [0, 1., 0], [1., 0, 0], [0, 1., 0], [1., 0, 0], [0, 1., 0]])
+        initial = initial.astype('float32')
+        initial = initial.flatten()
+
+        net = tf.layers.dense(net, 18, activation=tf.nn.tanh, bias_initializer=tf.initializers.constant(initial))
+
+        bg, title, credit = tf.split(inputs, [3, 4, 4], 3)
+        bg_p, title_p, credit_p = tf.split(net, 3, 1)
+        bg_p[1] = 0
+        bg_p[3] = 0
+        bg_p[4] = bg_p[0]
+        title_p[1] = 0
+        title_p[3] = 0
+        title_p[4] = title_p[0]
+        credit_p[1] = 0
+        credit_p[3] = 0
+        credit_p[4] = credit_p[0]
         bg_trans = transformer(bg, bg_p, (out_height, out_width))
         title_trans = transformer(title, title_p, (out_height, out_width))
         credit_trans = transformer(credit, credit_p, (out_height, out_width))
